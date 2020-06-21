d2224db6d1c40f385f28edd99795f3ae086f714f
sonata-chen <59016625+sonata-chen@users.noreply.github.com>
2020-06-21 11:48:04 +0800

    Use Legacy BIOS

--- install.sh
+++ install.sh
@@ -1,38 +1,21 @@
 #!/bin/sh
 
-EFI_partition=
-EFI_mount_point=/efi
+
 ROOT_partition=
-HOME_partition=
-UEFI=true
+
 
 timedatectl set-ntp true
 
-# echo PARTED
-# parted $DEVICE mklabel msdos
-# parted $DEVICE mkpart primary ext4 1MiB 500MiB
-# parted $DEVICE mkpart primary ext4 500MiB 15GiB
-# parted $DEVICE set 1 boot on
 
 echo -e "\e[1;36m\n------ hardrive setup ------\n\e[0m"
 
 mkfs.ext4 ${ROOT_partition} 
-mkfs.ext4 ${HOME_partition}
 
-read -p $'\e[1;36mDo you want to format efi partion? [y\N] \e[0m' ask_efi_format
-ask_efi_format=${ask_efi_format:-n}
-if [[ $ask_efi_format =~ ^[Yy]$ ]]
-then
-   echo
-   mkfs.fat -F32 ${EFI_partition} 
-fi
+
 
 
 mount ${ROOT_partition} /mnt
-mkdir /mnt/home
-mkdir /mnt${EFI_mount_point}
-mount ${EFI_partition} /mnt/efi
-mount ${HOME_partition} /mnt/home
+
 
 
 echo -e "\e[1;36m\n------ mirrorlist setup ------\n\e[0m"
